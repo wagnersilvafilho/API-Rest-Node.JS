c06580ead0ebd67de6bfb5823cd16a2db4bec4cf
wagnersilvafilho <wagnersf97@gmail.com>
2021-06-30 22:23:51 -0300

    Persistindo dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
 
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando get'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('Você está na rota de atendimentos e será gerado um post!')
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
     })
 }
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/Tabelas')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
+        Tabelas.init(conexao)
+        const app = customExpress()
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+})
 
