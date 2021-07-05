a8a49cc5c63b517f96120bf5fcbdc3af980176f6
wagnersilvafilho <wagnersf97@gmail.com>
2021-07-04 22:20:09 -0300

    Acrescentando funcionalidades e restrições para adição de elementos

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,15 +1,40 @@
+const moment = require("moment")
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento){
-        const sql = 'INSERT INTO Atendimentos SET ?'
-        conexao.query(sql, atendimento, (erro, resultados) => {
+    adiciona(atendimento, res){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.lengh >= 5
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
+            }
+        ]
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+        console.log(existemErros)
+        if(existemErros){
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro){
-                console.log
+               res.status(400).json(erro)
             } else {
-                console.log(resultados)
+                res.status(201).json(resultados)
             }
         })
+        }
     }
 }
 
