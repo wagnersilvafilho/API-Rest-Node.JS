96b835a8de673e2336d2b210ddd89661bbee4fb2
wagnersilvafilho <wagnersf97@gmail.com>
2021-07-15 10:24:48 -0300

    adição metodos http e correção bug validação post

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,7 +6,7 @@ class Atendimento {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
         const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
-        const clienteEhValido = atendimento.cliente.lengh >= 5
+        const clienteEhValido = atendimento.cliente.length >= 5
         const validacoes = [
             {
                 nome: 'data',
@@ -31,10 +31,61 @@ class Atendimento {
             if (erro){
                res.status(400).json(erro)
             } else {
-                res.status(201).json(resultados)
+                res.status(201).json(atendimento)
             }
         })
         }
+    }
+
+    lista(res){
+        const sql = `SELECT * FROM Atendimentos`
+        conexao.query(sql, (erro, resultados) => {
+             if (erro){
+                 res.status(400).json(erro)
+             } else {
+                 res.status(200).json(resultados)
+             }
+        }
+        )
+    }
+
+    buscaPorId(id, res){
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0]
+            if (erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    altera(id, valores, res){
+        if (valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
+
+        const sql = `UPDATE Atendimentos SET ? WHERE id=?`
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta(id, res){
+        const sql = `DELETE FROM Atendimentos WHERE id=?`
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
     }
 }
 
